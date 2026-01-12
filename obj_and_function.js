d88bcb0d972edb93bd0fca49e8d8944af8c34f0d
learner <nehaverma5567@gmail.com>
2026-01-12 18:02:41 +0530

    get_array function added

--- obj_and_function.js
+++ obj_and_function.js
@@ -3,6 +3,7 @@ const new_object = {
     features : "object_behaviour"
 }
 
+// Argument Passing
 function to_print_sum(num1, num2){
 
     return console.log('sum function',num1+num2);   //output :sum function 15
@@ -11,3 +12,18 @@ function to_print_sum(num1, num2){
 to_print_sum(10,5);
 
 console.log(new_object);  //output: { name: 'object_name', features: 'object_behaviour' }
+
+//Array
+function get_array(arry , index){
+
+    console.log(`Value at index ${index} is : ${arry[index]}`); //output: Value at index 2 is : 30
+
+    console.log(`array length : ${arry.length}`); //output: array length : 5
+
+
+}
+
+const arry = [10,20,30,40,50];
+
+get_array(arry ,2);
+
